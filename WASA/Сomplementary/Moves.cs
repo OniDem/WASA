45ce0bcbe12a71fe98245b4fd962ff3cad65d8ba
OniDem1 <spaindecraft17@gmail.com>
2023-06-01 11:52:01 +0700

    Убрал неиспользуемые директивы

--- WASA/Сomplementary/Moves.cs
+++ WASA/Сomplementary/Moves.cs
@@ -1,18 +1,8 @@
 ﻿using Npgsql;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
-using System.Windows.Controls.Primitives;
 using System.Windows.Media;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace WASA.Сomplementary
 {
