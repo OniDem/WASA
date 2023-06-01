aa0147a873e8c6f9645ca047c853e10e20287573
OniDem1 <spaindecraft17@gmail.com>
2023-06-01 13:05:43 +0700

    Убрал поддержку кнопок сортировки

--- WASA/Сomplementary/Balance_Changes.cs
+++ WASA/Сomplementary/Balance_Changes.cs
@@ -19,8 +19,7 @@ namespace WASA.Сomplementary
         UI_Updates? updates = new UI_Updates();
         Selected_Type? selected =new Selected_Type();
         string? internal_article;
-        public void Balance_Change(TextBox change_external_article, TextBox change_internal_article, int _count, string current_user, Label UserUI_Label_RealTime , Button Select_All,
-            DataGrid dataGrid, Button Sort_Article, Button Sort_Name, Button Sort_Price, Button Sort_Balance, Button Sort_Add_man, Button Sort_Change_Text, Button Sort_Type, string selected_type)
+        public void Balance_Change(TextBox change_external_article, TextBox change_internal_article, int _count, string current_user, Label UserUI_Label_RealTime, DataGrid dataGrid)
         {
             con = new NpgsqlConnection(Connection.GetConnectionString());
             con.Open();
@@ -38,16 +37,7 @@ namespace WASA.Сomplementary
             command = new NpgsqlCommand($"UPDATE products SET change='{current_user + " " + UserUI_Label_RealTime.Content}' WHERE internal_article='{internal_article}';", con);
             command.ExecuteNonQuery();
             con.Close();
-            if (Select_All.Background != Brushes.Aqua)
-            {
-
-                updates!.UI_Update(dataGrid, selected!.Selected(Sort_Article, Sort_Name, Sort_Price, Sort_Balance, Sort_Add_man, Sort_Change_Text, Sort_Type,
-                   $"SELECT * FROM products WHERE product_type = '{selected_type}' ORDER BY "));
-            }
-            else
-            {
-                updates!.UI_Update(dataGrid, $"SELECT * FROM products  ORDER BY internal_article;");
-            }
+                updates!.UI_Update(dataGrid, $"SELECT * FROM products;");
         }
     }
 }
