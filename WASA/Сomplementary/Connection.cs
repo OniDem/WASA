034872f087f3860e813916128c33f8295d14250b
Александр Демидов <Spaindecraft17@gmail.com>
2023-06-08 20:51:59 +0700

    Изменил проверку на MAC вместо IP так же убрал неиспользуемые конструкторы

--- WASA/Сomplementary/Connection.cs
+++ WASA/Сomplementary/Connection.cs
@@ -1,20 +1,20 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
 namespace WASA.Сomplementary
 {
     internal class Connection
     {
-        static String host = System.Net.Dns.GetHostName();
-
-
         public static string GetConnectionString()
         {
-            var httpClient = new HttpClient();
+            var macAddr = (from nic in NetworkInterface.GetAllNetworkInterfaces() where nic.OperationalStatus == OperationalStatus.Up select nic.GetPhysicalAddress().ToString()).FirstOrDefault();
+            
 
-            if (Properties.Resources.host == GetLocalIPAddress())
+            if (Properties.Resources.mac == macAddr)
             {
                 return $"Host=localhost;Port=5432;DataBase=wasa;Username=postgres;Password=1234";
             }
@@ -22,29 +22,6 @@ namespace WASA.Сomplementary
             {
                 return $"Host='5.137.212.65';Port=5432;DataBase=wasa;Username=postgres;Password=1234";
             }
-        }
-
-        public static string GetConnectionString(string username, string password)
-        {
-            return $"host='{host}';port=5432;database=wasa;username='{username}';password='{password}'";
-        }
-
-        public static string GetConnectionString(string Ip, string Port, string DataBaseName, string Username, string Password)
-        {
-            return $"Host='{host}';Port='{Port}';DataBase='{DataBaseName}';Username='{Username}';Password='{Password}'";
-        }
-
-        private static string GetLocalIPAddress()
-        {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    return ip.ToString();
-                }
-            }
-            throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
 }
