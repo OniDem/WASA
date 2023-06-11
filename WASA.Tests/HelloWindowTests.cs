f2033a310707e47fda1889cf0b9c6053a37b7036
OniDem1 <spaindecraft17@gmail.com>
2023-06-11 20:14:57 +0700

    Добавил шаблон автоматического теста для HelloWindow

---
+++ WASA.Tests/HelloWindowTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WASA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASA.Tests
+{
+    [TestClass()]
+    public class HelloWindowTests
+    {
+        [TestMethod()]
+        public void HelloWindowTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
