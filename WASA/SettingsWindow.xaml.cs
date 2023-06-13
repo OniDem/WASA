f326e9e223c5397f99d70ea3e689d3cb9c426ad0
OniDem1 <spaindecraft17@gmail.com>
2023-06-13 15:54:20 +0700

    Добавил просмотр текущего MAC адреса

--- WASA/SettingsWindow.xaml.cs
+++ WASA/SettingsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Net.NetworkInformation;
+using System.Windows;
 
 namespace WASA
 {
@@ -10,11 +11,37 @@ namespace WASA
         public SettingsWindow()
         {
             InitializeComponent();
+            MAC_Address.Text = GetMacAddress();
         }
 
-        private void apply_Click(object sender, RoutedEventArgs e)
+
+        private void Check_Click(object sender, RoutedEventArgs e)
         {
-            
+            MAC_Address.Text = GetMacAddress();
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            HelloWindow helloWindow = new HelloWindow();
+            helloWindow.Show();
+            Close();
+        }
+
+        static string GetMacAddress()
+        {
+            string macAddresses = "";
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // Only consider Ethernet network interfaces, thereby ignoring any
+                // loopback devices etc.
+                if (nic.NetworkInterfaceType != NetworkInterfaceType.Ethernet) continue;
+                if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+            return macAddresses;
         }
     }
 }
