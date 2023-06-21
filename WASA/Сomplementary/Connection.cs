0ef9f4f3b74596493f866eb9b07846d6c565caf8
OniDem1 <spaindecraft17@gmail.com>
2023-06-21 10:47:08 +0700

    Сделал проверку mac адресов

--- WASA/Сomplementary/Connection.cs
+++ WASA/Сomplementary/Connection.cs
@@ -1,29 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Windows.Documents;
 
 namespace WASA.Сomplementary
 {
     internal class Connection
     {
-        private const string _mac = "B42E9966DBE8";
+        private static string[] _mac = new string[] { "B42E9966DBE8", "3C219C83D01F" };
         private const string _ip = "90.189.146.84";
         public static string GetConnectionString()
         {
             var macAddr = (from nic in NetworkInterface.GetAllNetworkInterfaces() where nic.OperationalStatus == OperationalStatus.Up select nic.GetPhysicalAddress().ToString()).FirstOrDefault();
-            
 
-            if (_mac == macAddr)
+
+            foreach (var mac in _mac)
             {
-                return $"Host=localhost;Port=5432;DataBase=wasa;Username=postgres;Password=1234";
+                if (mac != macAddr)
+                {
+                    return $"Host='{_ip}';Port=5432;DataBase=wasa;Username=postgres;Password=1234";
+                }
             }
-            else
-            {
-                return $"Host='{_ip}';Port=5432;DataBase=wasa;Username=postgres;Password=1234";
-            }
+            return $"Host=localhost;Port=5432;DataBase=wasa;Username=postgres;Password=1234";
         }
     }
 }
