6aeb84075da6ac6204c03febb8ab3b3c2a8fc1b2
OniDem1 <spaindecraft17@gmail.com>
2023-06-24 10:38:19 +0700

    Вернул назад

--- WASA/Сomplementary/UserInfo.cs
+++ WASA/Сomplementary/UserInfo.cs
@@ -7,26 +7,27 @@ namespace WASA.Сomplementary
     internal class UserInfo
     {
         NpgsqlCommand? command;
-        private string _current_user = "";
-        public void SetCurrenUser(string user)
-        {
-            _current_user = user;
-            
-        }
-
         public string GetCurrentUser()
         {
-            return _current_user;
+            NpgsqlConnection con = new NpgsqlConnection(Connection.GetConnectionString());
+            con!.Open();
+            command = new NpgsqlCommand($"SELECT seller FROM settings WHERE settings_id = 1", con);
+            string? user = Convert.ToString(command.ExecuteScalar());
+            con.Close();
+            return user!;
+
         }
 
         public string GetUserRole()
         {
-                NpgsqlConnection con = new NpgsqlConnection(Connection.GetConnectionString());
-                con!.Open();
-                command = new NpgsqlCommand($"SELECT user_role FROM users WHERE user_name = '{_current_user}'", con);
-                string? user_role = Convert.ToString(command.ExecuteScalar());
-                con.Close();
-                return user_role!;
+            NpgsqlConnection con = new NpgsqlConnection(Connection.GetConnectionString());
+            con!.Open();
+            command = new NpgsqlCommand($"SELECT seller FROM settings WHERE settings_id = 1", con);
+            string? user = Convert.ToString(command.ExecuteScalar());
+            command = new NpgsqlCommand($"SELECT user_role FROM users WHERE user_name = '{user}'", con);
+            string? user_role = Convert.ToString(command.ExecuteScalar());
+            con.Close();
+            return user_role!;
         }
     }
 }
