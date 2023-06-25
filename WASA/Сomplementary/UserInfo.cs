ee1b3bb1bc47e1a1c8a15ae8026bfe4b6b4446f9
OniDem1 <spaindecraft17@gmail.com>
2023-06-25 11:46:08 +0700

    Уменьшил вложенность классов

--- WASA/Сomplementary/UserInfo.cs
+++ WASA/Сomplementary/UserInfo.cs
@@ -1,6 +1,5 @@
 ﻿using Npgsql;
 using System;
-using System.Windows;
 
 namespace WASA.Сomplementary
 {
@@ -18,12 +17,10 @@ namespace WASA.Сomplementary
 
         }
 
-        public string GetUserRole()
+        public string GetUserRole(string user)
         {
             NpgsqlConnection con = new NpgsqlConnection(Connection.GetConnectionString());
             con!.Open();
-            command = new NpgsqlCommand($"SELECT seller FROM settings WHERE settings_id = 1", con);
-            string? user = Convert.ToString(command.ExecuteScalar());
             command = new NpgsqlCommand($"SELECT user_role FROM users WHERE user_name = '{user}'", con);
             string? user_role = Convert.ToString(command.ExecuteScalar());
             con.Close();
