bb2282ff94b49384ee7d95763a4df19e1d7a220c
OniDem1 <spaindecraft17@gmail.com>
2023-06-25 11:44:37 +0700

    Добавил асинхронный метод обновления

--- WASA/Сomplementary/UI_Updates.cs
+++ WASA/Сomplementary/UI_Updates.cs
@@ -3,7 +3,6 @@ using System;
 using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Animation;
 
 namespace WASA.Сomplementary
 {
@@ -12,11 +11,11 @@ namespace WASA.Сomplementary
         private NpgsqlCommand? command;
 
 
-        public void UI_Update(DataGrid dataGrid, string sql)
+        public void UI_Update(DataGrid dataGrid, string sql, NpgsqlConnection con)
         {
             try
             {
-                NpgsqlConnection con = new NpgsqlConnection(Connection.GetConnectionString());
+               // NpgsqlConnection con = new NpgsqlConnection(Connection.GetConnectionString());
                 con.Open();
                 DataTable dt = new DataTable();
                 command = new NpgsqlCommand(sql, con);
@@ -33,19 +32,54 @@ namespace WASA.Сomplementary
 
         }
 
+        public async void UI_UpdateAsync(TextBox delete_id, Button delete, TextBlock all_cash, TextBlock all_aq, TextBlock all, DataGrid dataGrid, string sql, int dayofyear, NpgsqlConnection con)
+        {
+            try
+            {
+                await con.OpenAsync();
+                DataTable dt = new DataTable();
+                command = new NpgsqlCommand(sql, con);
+                await command.ExecuteNonQueryAsync();
+                dt.Load(await command.ExecuteReaderAsync(CommandBehavior.CloseConnection));
+                dataGrid.DataContext = dt;
+                dataGrid.DataContext = dt.DefaultView;
+                command = new NpgsqlCommand($"SELECT id FROM sale WHERE shift = '{dayofyear}' ORDER BY id DESC", con);
+                delete_id.Text = Convert.ToString(await command.ExecuteScalarAsync());
+                command = new NpgsqlCommand($"SELECT cash FROM accounting WHERE shift = '{dayofyear}'", con);
+                all_cash.Text = Convert.ToString(await command.ExecuteScalarAsync());
+                command = new NpgsqlCommand($"SELECT acquiring FROM accounting WHERE shift = '{dayofyear}'", con);
+                all_aq.Text = Convert.ToString(await command.ExecuteScalarAsync());
+                command = new NpgsqlCommand($"SELECT shift_sum FROM accounting WHERE shift = '{dayofyear}'", con);
+                all.Text = Convert.ToString(await command.ExecuteScalarAsync());
+                await con.CloseAsync();
+
+                delete.IsEnabled = false;
+
+                if (delete_id.Text != "")
+                {
+                    delete.IsEnabled = true;
+
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         public void UI_Update(TextBox delete_id, Button delete, TextBlock all_cash, TextBlock all_aq, TextBlock all, DataGrid dataGrid, string sql, int dayofyear)
         {
             try
             {
                 NpgsqlConnection con = new NpgsqlConnection(Connection.GetConnectionString());
-                
-                UI_Update(dataGrid, sql);
+
+                UI_Update(dataGrid, sql, con);
 
                 con.Open();
                 command = new NpgsqlCommand($"SELECT id FROM sale WHERE shift = '{dayofyear}' ORDER BY id DESC", con);
                 delete_id.Text = Convert.ToString(command.ExecuteScalar());
                 command = new NpgsqlCommand($"SELECT cash FROM accounting WHERE shift = '{dayofyear}'", con);
-                all_cash.Text = Convert.ToString(command.ExecuteScalar());
+                all_cash.Text = Convert.ToString( command.ExecuteScalar());
                 command = new NpgsqlCommand($"SELECT acquiring FROM accounting WHERE shift = '{dayofyear}'", con);
                 all_aq.Text = Convert.ToString(command.ExecuteScalar());
                 command = new NpgsqlCommand($"SELECT shift_sum FROM accounting WHERE shift = '{dayofyear}'", con);
