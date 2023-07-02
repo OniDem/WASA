57436c7a306457613c83dc112b23435ec3391b2e
OniDem1 <spaindecraft17@gmail.com>
2023-07-02 11:37:58 +0700

    Вернул назад

--- WASA.DevTools/Program.cs
+++ WASA.DevTools/Program.cs
@@ -13,38 +13,14 @@ namespace ConsoleApp5
 
         static void Main(string[] args)
         {
-            //Console.WriteLine(GetMacAddress());
-            Process proc = new Process();
-            proc.StartInfo.CreateNoWindow = true;
-            proc.StartInfo.FileName = "cmd";
-
-            proc.StartInfo.Arguments = @"/C ""arp 192.168.0.1 -a""";
-
-            proc.StartInfo.RedirectStandardOutput = true;
-            proc.StartInfo.UseShellExecute = false;
-            proc.Start();
-            string output = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-
-            Console.WriteLine(output);
+            Console.WriteLine(GetMacAddress());            
             Console.WriteLine();
             GetSerialPorts();
         }
         static string GetMacAddress()
         {
-            string macAddresses = "";
-            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                // Only consider Ethernet network interfaces, thereby ignoring any
-                // loopback devices etc.
-                if (nic.NetworkInterfaceType != NetworkInterfaceType.Ethernet) continue;
-                if (nic.OperationalStatus == OperationalStatus.Up)
-                {
-                    macAddresses += nic.GetPhysicalAddress().ToString();
-                    break;
-                }
-            }
-            return macAddresses;
+            var macAddr = (from nic in NetworkInterface.GetAllNetworkInterfaces() where nic.OperationalStatus == OperationalStatus.Up select nic.GetPhysicalAddress().ToString()).FirstOrDefault();
+            return macAddr!;
         }
 
         static void GetSerialPorts()
