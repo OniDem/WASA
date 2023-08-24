b14e8c03d1d726a0e1ccda454bfd07bbcb238df9
OniDem1 <spaindecraft17@gmail.com>
2023-08-24 13:27:24 +0700

    Починил подключение внутри локальной сети

--- WASA/Сomplementary/Connection.cs
+++ WASA/Сomplementary/Connection.cs
@@ -1,29 +1,34 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net.NetworkInformation;
+using System.Windows;
 
 namespace WASA.Сomplementary
 {
     internal class Connection
     {
-        private static string[] _mac = new[] { "B42E9966DBE8", "3C219C83D01F" };
+        private static string[] _mac = new[] { "B42E9966DBE8", "3E219C83D01F" };
         private const string _ip = "90.189.146.84";
         public static string GetConnectionString()
         {
-            var macAddr = (from nic in NetworkInterface.GetAllNetworkInterfaces() where nic.OperationalStatus == OperationalStatus.Up select nic.GetPhysicalAddress().ToString()).FirstOrDefault();
-
-
-            foreach (var mac in _mac)
+            try
             {
-                if (mac != macAddr)
+                var macAddr = (from nic in NetworkInterface.GetAllNetworkInterfaces() where nic.OperationalStatus == OperationalStatus.Up select nic.GetPhysicalAddress().ToString()).FirstOrDefault();
+                foreach (var mac in _mac)
                 {
-                    return $"Host='{_ip}';Port=5432;DataBase=wasa;Username=postgres;Password=1234";
+                    if (mac == macAddr)
+                    {
+                        return $"Host=192.168.0.6;Port=5432;DataBase=wasa;Username=postgres;Password=1234";
+                        
+                    }
+                   
                 }
-                else
-                {
-                    return $"Host=localhost;Port=5432;DataBase=wasa;Username=postgres;Password=1234";
-                }
+                return $"Host='{_ip}';Port=5432;DataBase=wasa;Username=postgres;Password=1234";
             }
-            return null!;
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }
